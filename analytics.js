4260f15d85503b8d70e12c2af58717ebbb075d68
Jonathan Ferguson <jono@spiralarm.com>
2017-07-27 17:04:51 +1000

    Major elements we want for a landing page, with alanalytics

--- analytics.js
+++ analytics.js
@@ -1,5 +1,5 @@
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (function(i,s,o,g,r,a,m){
+    i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
@@ -7,4 +7,14 @@
   ga('create', 'UA-29444727-2', 'auto');
   ga('send', 'pageview');
 
-</script>
+  function handleDownloadClicks(name, format) {
+    ga('send', 'event', {
+      eventCategory: 'Download',
+      eventAction: name,
+      eventLabel: format,
+      transport: 'beacon'
+    });
+    return false;
+  }
+
+
